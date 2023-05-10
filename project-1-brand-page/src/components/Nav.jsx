94b1a3e2d4d676ce4d14d9fd9a12ec3a6af7330b
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-05-10 12:57:36 +0600

    project-1 completed

---
+++ project-1-brand-page/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+const Nav = () => {
+    return (
+        <nav className="container">
+            <div className="logo">
+                <img src="/images/brand_logo.png" alt="" />
+            </div>
+            <ul>
+                <li>Menu</li>
+                <li>Location</li>
+                <li>About </li>
+                <li>Contact</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    )
+}
+export default Nav;
